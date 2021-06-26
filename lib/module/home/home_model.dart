a354d77fa9ed569a5a7b01cf17b269230dc8891a
taivnguyen <taivnguyen@lecle.co.kr>
2021-06-26 19:23:57 +0700

    update view

--- lib/module/home/home_model.dart
+++ lib/module/home/home_model.dart
@@ -2,25 +2,65 @@ import 'dart:convert';
 import 'dart:math';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:pluto_grid/pluto_grid.dart';
 
 class HomeModel extends ChangeNotifier {
-  final data = <String, dynamic>{};
   String fileName = '';
+  final Map<String, dynamic> categories = {};
+  final Map<String, dynamic> items = {};
+
+  final List<PlutoColumn> cols = [];
+  final List<PlutoRow> rows = [];
+
+  HomeModel() {
+    _initCols();
+  }
+
   void pick() async {
     FilePickerResult result = await FilePicker.platform.pickFiles(
       type: FileType.custom,
       allowedExtensions: ['json'],
     );
-
     if (result != null) {
       PlatformFile f = result.files.single;
       fileName = '${f.name} - ${formatBytes(f.size, 2)}';
       String d = String.fromCharCodes(f.bytes);
       final json = jsonDecode(d) as Map;
-      data.clear();
-      data.addAll(json);
+      categories.clear();
+      categories.addAll(json['categories']);
+
+      items.clear();
+      final ls = json['items'] as List;
+      final map = Map.fromIterable(ls, key: (e) => e['id'].toString(), value: (e) => e);
+      items.addAll(map);
       notifyListeners();
     }
+  }
+
+  void _initCols() {
+    cols.clear();
+    cols.addAll([
+      PlutoColumn(
+        title: 'ID',
+        field: 'id',
+        type: PlutoColumnType.text(),
+      ),
+      PlutoColumn(
+        title: 'category',
+        field: 'number_field',
+        type: PlutoColumnType.number(),
+      ),
+      PlutoColumn(
+        title: 'date column',
+        field: 'date_field',
+        type: PlutoColumnType.date(),
+      ),
+      PlutoColumn(
+        title: 'time column',
+        field: 'time_field',
+        type: PlutoColumnType.time(),
+      ),
+    ]);
   }
 }
 
--- lib/module/home/home_view.dart
+++ lib/module/home/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pluto_grid/pluto_grid.dart';
 import 'package:provider/provider.dart';
 import 'home_model.dart';
 
@@ -21,7 +22,7 @@ class _HomeViewState extends State<_HomeView> {
   Widget build(BuildContext context) {
     final model = context.watch<HomeModel>();
     return Scaffold(
-      backgroundColor: Colors.grey,
+      // backgroundColor: Colors.grey,
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
@@ -29,6 +30,8 @@ class _HomeViewState extends State<_HomeView> {
             buildPicker(model),
             SizedBox(height: 16),
             buildCategory(model),
+            SizedBox(height: 16),
+            buildTable(model),
           ],
         ),
       ),
@@ -41,10 +44,7 @@ class _HomeViewState extends State<_HomeView> {
       children: [
         MaterialButton(
           onPressed: model.pick,
-          child: Text(
-            'Pick',
-            style: TextStyle(color: Colors.black),
-          ),
+          child: Text('Select file'),
         ),
         SizedBox(width: 16),
         Text('${model.fileName}'),
@@ -53,10 +53,52 @@ class _HomeViewState extends State<_HomeView> {
   }
 
   Widget buildCategory(HomeModel model) {
-    if (model.data.isEmpty) return Container();
+    if (model.categories.isEmpty) return Container();
+    final List<Widget> ls = [];
+    model.categories.forEach((k, v) {
+      ls.add(Padding(
+        padding: const EdgeInsets.all(4),
+        child: Column(
+          children: [
+            Text(k),
+            Spacer(),
+            Text(v),
+          ],
+        ),
+      ));
+    });
     return Container(
-      color: Colors.amber,
-      height: 100,
+      height: 50,
+      decoration: BoxDecoration(border: Border.all()),
+      child: ListView.separated(
+        scrollDirection: Axis.horizontal,
+        shrinkWrap: true,
+        itemCount: model.categories.length,
+        separatorBuilder: (_, __) => VerticalDivider(color: Colors.black, width: 1),
+        itemBuilder: (_, i) {
+          return Padding(
+            padding: const EdgeInsets.all(4),
+            child: Column(
+              children: [
+                Text(model.categories.keys.toList()[i]),
+                Spacer(),
+                Text(model.categories.values.toList()[i]),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget buildTable(HomeModel model) {
+    if (model.items.isEmpty) return Container();
+    return Expanded(
+      child: PlutoGrid(
+        columns: model.cols,
+        rows: model.rows,
+        configuration: PlutoGridConfiguration.dark(),
+      ),
     );
   }
 }
